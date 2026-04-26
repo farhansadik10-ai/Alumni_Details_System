74f85a3683c56639097919197d41c3d61f6f8299
farhansadik10-ai <fsadik906@gmail.com>
2026-04-27 01:22:24 +0600

    create shared types for alumni, comment, posts and user

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import pool from './config/db';
 
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+  } else {
+    console.log('Database connection successful:', res.rows[0]);
+  }
+});
 dotenv.config();
 
 const app = express();
